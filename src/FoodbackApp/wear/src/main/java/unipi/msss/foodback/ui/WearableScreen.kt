9ccf426ba9a1488ce694f8be655c33e6c42b4081
Tommaso Califano <141735003+Califfo8@users.noreply.github.com>
2025-05-12 12:22:26 +0200

    feat: Wearable GUI improvements
    - Reduced the spacing between sensor values.
    - Implemented GUI feedback when the wearable is collecting data.

--- src/FoodbackApp/wear/src/main/java/unipi/msss/foodback/ui/WearableScreen.kt
+++ src/FoodbackApp/wear/src/main/java/unipi/msss/foodback/ui/WearableScreen.kt
@@ -1,16 +1,20 @@
 package unipi.msss.foodback.ui
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -22,6 +26,7 @@ import com.airbnb.lottie.compose.LottieConstants
 import com.airbnb.lottie.compose.rememberLottieComposition
 import unipi.msss.foodback.R
 import unipi.msss.foodback.viewmodel.WearableViewModel
+import kotlin.math.min
 
 
 //import androidx.compose.ui.platform.LocalContext
@@ -38,54 +43,84 @@ fun WearableScreen(viewModel: WearableViewModel = viewModel()) {
             .background(MaterialTheme.colors.background),
         contentAlignment = Alignment.Center
     ) {
+        if (state.isCollecting) {
+            RedBorderOverlay()
+        }
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(12.dp) // Spaziatura tra gli elementi
+            verticalArrangement = Arrangement.spacedBy(12.dp) // Spacing between elements
         ) {
-             // EDA Animation
-            LottieAnimation(
-                composition = edaComposition,
-                modifier = Modifier.size(50.dp),
-                iterations = LottieConstants.IterateForever
-            )
+            Column (
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                // EDA Animation
+                LottieAnimation(
+                    composition = edaComposition,
+                    modifier = Modifier.size(50.dp),
+                    iterations = LottieConstants.IterateForever
+                )
 
-            // EDA value
-            state.latestEda?.let { eda->
-                Text(
-                    text = "${eda.toInt()} µS",
+                // EDA value
+                state.latestEda?.let { eda->
+                    Text(
+                        text = "${eda.toInt()} µS",
+                        style = MaterialTheme.typography.title3,
+                        color = MaterialTheme.colors.onBackground,
+                        textAlign = TextAlign.Center
+                    )
+                } ?: Text(
+                    text = "No Data",
                     style = MaterialTheme.typography.title3,
                     color = MaterialTheme.colors.onBackground,
                     textAlign = TextAlign.Center
                 )
-            } ?: Text(
-                text = "No Data",
-                style = MaterialTheme.typography.title3,
-                color = MaterialTheme.colors.onBackground,
-                textAlign = TextAlign.Center
-            )
+            }
+            Column (
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                // HeartRate Animation
+                LottieAnimation(
+                    composition = hrComposition,
+                    modifier = Modifier.size(50.dp),
+                    iterations = LottieConstants.IterateForever
+                )
 
-            // HeartRate Animation
-            LottieAnimation(
-                composition = hrComposition,
-                modifier = Modifier.size(50.dp),
-                iterations = LottieConstants.IterateForever
-            )
-
-            // HeartRate value
-            state.latestHeartRate?.let { bpm ->
-                Text(
-                    text = "${bpm.toInt()} BPM",
+                // HeartRate value
+                state.latestHeartRate?.let { bpm ->
+                    Text(
+                        text = "${bpm.toInt()} BPM",
+                        style = MaterialTheme.typography.title3,
+                        color = MaterialTheme.colors.onBackground,
+                        textAlign = TextAlign.Center
+                    )
+                } ?: Text(
+                    text = "No data",
                     style = MaterialTheme.typography.title3,
                     color = MaterialTheme.colors.onBackground,
                     textAlign = TextAlign.Center
                 )
-            } ?: Text(
-                text = "No data",
-                style = MaterialTheme.typography.title3,
-                color = MaterialTheme.colors.onBackground,
-                textAlign = TextAlign.Center
-            )
+            }
 
+
+        }
+    }
+}
+
+@Composable
+fun RedBorderOverlay() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(4.dp) // Internal margin, to not cut the edge
+    ) {
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            val diameter = min(size.width, size.height)
+            val strokeWidth = 2.dp.toPx()
+            drawCircle(
+                color = Color.Red,
+                radius = diameter / 2 - strokeWidth / 2,
+                style = Stroke(width = strokeWidth)
+            )
         }
     }
 }
